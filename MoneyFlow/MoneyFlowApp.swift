c659bf51363d595a88503910b12cef838ae2a2c1
Sadat Ahmed <sadatahmed52@gmail.com>
2024-11-20 17:05:44 +0600

    Initial Commit

---
+++ MoneyFlow/MoneyFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyFlowApp.swift
+//  MoneyFlow
+//
+//  Created by Sadat Ahmed on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MoneyFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
